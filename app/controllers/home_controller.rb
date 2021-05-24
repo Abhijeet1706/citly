82f3922d2022d2761e82a8235dcff2475d740879
Abhijeet1706 <abhijeet.singh1706@gmail.com>
2021-05-24 19:48:46 +0530

    updated home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,21 @@
 class HomeController < ApplicationController
+  before_action :load_link_by_shortened, only: :show
+
+  def index 
+    render
+  end
+
+  def show
+    if @link.present?
+      @link.increment!(:clicks)
+      redirect_to @link.link
+    else
+      render
+    end
+  end
+
+  private
+    def load_link_by_shortened
+      @link = Link.find_by(shortened: params[:id])
+    end
 end
